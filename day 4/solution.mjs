3f95bba5fee0bc16f46c32da697b2c72fa123a93
Luki1235512 <Luki1235512@gmail.com>
2022-12-09 12:17:15 +0100

    Day 4

--- day 4/solution.mjs
+++ day 4/solution.mjs
@@ -27,7 +27,22 @@ function part1() {
 
 
 function part2() {
+    const res = lines.map((line) => {
+        const [first, second] = line
+            .split(",")
+            .map((interval) => interval.split("-").map(Number))
+            .sort((a, b) => {
+                const oneSize = a[1] - a[0];
+                const twoSize = b[1] - b[0];
+                return twoSize - oneSize;
+            });
 
+        const overlap = first[1] >= second[0] && second[1] >= first[0];
+
+        return overlap ? 1 : 0;
+    });
+
+    console.log(res.reduce((a, b) => a + b, 0));
 }
 
 part1();
